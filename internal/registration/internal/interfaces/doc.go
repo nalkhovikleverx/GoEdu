57b9bc7e2b376ab139651fa2fb447b370590e2a9
Nikita Alkhovik <nikita.alkhovik@leverx.com>
2024-06-03 20:58:34 +0300

    Add registration module interfaces package placeholder.

---
+++ internal/registration/internal/interfaces/doc.go
@@ -0,0 +1,3 @@
+// Package interfaces implements the interface layer of the application.
+// The interface layer allows the application to decouple itself from the outside world.
+package interfaces
